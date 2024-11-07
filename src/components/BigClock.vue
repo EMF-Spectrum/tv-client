e2dfd35ddba0abe4b564754d287be5bad0f43bda
Lexi Robinson <lexi@lexi.org.uk>
2024-11-07 16:24:07 +0000

    Get the clock ticking
    Honestly it's pretty amazing what you can do when you hand things off to
    the CSS engine to handle! I look forward to this somehow being a
    performance bottleneck when it runs on whatever it runs on for the
    event, but it should still be kinda decent at 30fps?

--- src/components/BigClock.vue
+++ src/components/BigClock.vue
@@ -16,26 +16,246 @@
  You should have received a copy of the GNU General Public License
  along with The EMF Spectrum TV System.  If not, see <https://www.gnu.org/licenses/>.
 -->
+<script setup lang="ts">
+import { inject, ref } from "vue";
+
+import { HEARTBEAT_KEY } from "@/constants";
+
+const heartbeat = inject(HEARTBEAT_KEY)!;
+
+function timeBreakdown(num: DOMHighResTimeStamp): [number, number, number] {
+	let ms = num % 1000;
+	let temp = Math.floor(num / 1000);
+	let s = temp % 60;
+	let m = Math.floor(temp / 60);
+	return [m, s, ms];
+}
+
+function digit(num: number, which: number): string {
+	return (Math.floor(num / 10 ** which) % 10).toFixed(0);
+}
+
+const secondsTenthRef = ref("0");
+const secondsRef = ref("0");
+const tenSecondsRef = ref("0");
+const minutesRef = ref("0");
+const tenMinutesRef = ref("0");
+
+function animationFrame() {
+	window.requestAnimationFrame(animationFrame);
+	let status = heartbeat.timer;
+	if (status.state == "hidden") {
+		return;
+	}
+
+	let display: number;
+	if (status.state == "paused") {
+		display = status.timeLeft;
+	} else {
+		display = Math.max(0, status.endTime - Date.now());
+	}
+
+	let [m, s, ms] = timeBreakdown(display);
+
+	tenMinutesRef.value = digit(m, 1);
+	minutesRef.value = digit(m, 0);
+	tenSecondsRef.value = digit(s, 1);
+	secondsRef.value = digit(s, 0);
+	secondsTenthRef.value = digit(ms, 2);
+}
+window.requestAnimationFrame(animationFrame);
+</script>
+
 <template>
-	<main class="big-clock">
-		<canvas id="clock-canvas"></canvas>
+	<main
+		class="big-clock"
+		:class="{
+			'-paused': heartbeat.timer.state != 'running',
+			'-hidden': heartbeat.timer.state == 'hidden',
+		}"
+	>
+		<div class="number digit-holder">
+			<Transition>
+				<div class="digit" :key="tenMinutesRef">
+					{{ tenMinutesRef }}
+				</div>
+			</Transition>
+		</div>
+		<div class="number digit-holder">
+			<Transition>
+				<div class="digit" :key="minutesRef">
+					{{ minutesRef }}
+				</div>
+			</Transition>
+		</div>
+		<div class="separator">:</div>
+		<div class="number digit-holder">
+			<Transition>
+				<div class="digit" :key="tenSecondsRef">
+					{{ tenSecondsRef }}
+				</div>
+			</Transition>
+		</div>
+		<div class="number digit-holder">
+			<Transition>
+				<div class="digit" :key="secondsRef">
+					{{ secondsRef }}
+				</div>
+			</Transition>
+		</div>
+		<div class="separator">:</div>
+		<div class="number digit-holder">
+			<Transition>
+				<div class="digit -fast" :key="secondsTenthRef">
+					{{ secondsTenthRef }}
+				</div>
+			</Transition>
+		</div>
+		<!-- <div class="number digit-reel">
+			<div class="reel -tenths">
+				<div>8</div>
+				<div>9</div>
+				<div v-for="n in 10" :key="n">{{ n - 1 }}</div>
+				<div>0</div>
+				<div>1</div>
+			</div>
+		</div> -->
+		<div class="number digit-reel">
+			<div class="reel -hundredths">
+				<div>8</div>
+				<div>9</div>
+				<div v-for="n in 10" :key="n">{{ n - 1 }}</div>
+				<div>0</div>
+				<div>1</div>
+			</div>
+		</div>
 	</main>
 </template>
 
 <style lang="scss">
-canvas {
-	background: white;
-}
+$height: 250px;
 
 .big-clock {
 	& {
 		position: relative;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+
+		transition: opacity 0.5s;
 	}
 
-	> canvas {
+	&::after {
+		content: "";
 		position: absolute;
-		width: 100%;
-		height: 100%;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background: linear-gradient(
+			0deg,
+			white calc(45% - ($height/2)),
+			transparent calc(50% - ($height/2)),
+			transparent calc(50% + ($height/2)),
+			white calc(55% + ($height/2))
+		);
+		pointer-events: none;
+	}
+
+	&.-hidden {
+		opacity: 0;
+	}
+
+	> .number,
+	> .separator {
+		font-size: $height;
+		line-height: 1;
+		text-align: center;
+	}
+
+	> .separator {
+		flex: 0 0 auto;
+	}
+
+	> .number {
+		flex: 0 0 1em;
+	}
+}
+
+@keyframes digit-reel {
+	0% {
+		top: $height * -12;
+	}
+
+	100% {
+		top: $height * -2;
+	}
+}
+
+.digit-holder {
+	& {
+		position: relative;
+		height: $height;
+	}
+
+	> .digit {
+		& {
+			height: $height;
+			line-height: 1;
+			width: 1em;
+			text-align: center;
+		}
+
+		&.v-enter-active,
+		&.v-leave-active {
+			transition: top 0.2s ease-in-out;
+			position: absolute;
+
+			&.-fast {
+				transition-duration: 0.05s;
+			}
+		}
+
+		&.v-enter-from {
+			top: $height * -1;
+		}
+
+		&.v-enter-to,
+		&.v-leave-from {
+			top: 0px;
+		}
+
+		&.v-leave-to {
+			top: $height;
+		}
+	}
+}
+
+.digit-reel {
+	& {
+		position: relative;
+		height: $height;
+	}
+
+	> .reel {
+		position: absolute;
+		animation: digit-reel 0s linear 0s infinite;
+
+		.-paused & {
+			filter: none !important;
+			animation-play-state: paused;
+		}
+	}
+
+	> .reel.-tenths {
+		animation-duration: 1s;
+		filter: blur(1px);
+	}
+
+	> .reel.-hundredths {
+		animation-duration: 0.1s;
+		filter: blur(3px);
 	}
 }
 </style>
