b2200e6523dd14443eefcbac52f928c9f8e88e9f
Lexi Robinson <lexi@lexi.org.uk>
2024-11-08 22:27:45 +0000

    Fix up eslint some more
    Why does it not come with a default escape hatch for exceptions when
    it's mandatory to specify an identifier in catch blocks

--- eslint.config.js
+++ eslint.config.js
@@ -27,7 +27,10 @@ export default [
 			"prefer-const": "off",
 			"@typescript-eslint/no-unused-vars": [
 				"warn",
-				{ argsIgnorePattern: "^_" },
+				{
+					argsIgnorePattern: "^_",
+					caughtErrorsIgnorePattern: "^_",
+				},
 			],
 			"@typescript-eslint/no-explicit-any": "off",
 		},
