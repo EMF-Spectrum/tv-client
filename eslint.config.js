ce8a27307b93040769fd68e7725b8a136d17700b
Lexi Robinson <lexi@lexi.org.uk>
2024-11-08 15:49:33 +0000

    Fix more annoying rules

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,11 @@ export default [
 	{
 		rules: {
 			"prefer-const": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/no-explicit-any": "off",
 		},
 	},
 ];
